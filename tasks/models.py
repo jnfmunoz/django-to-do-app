ee589a67a8edd885a574a61dc427b61cb6de5fc1
jnfmunoz <juanmunozgrau@gmail.com>
2024-10-14 02:21:49 -0300

    commit-2

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Task(models.model):
+    titulo = models.CharField(min_length=100 ,max_length=200)
+    descripcion = models.TextField(min_length=100 ,max_length=200)
+    fecha_limite = models.DateField()
+    prioridad = models.CharField()
+    estado = models.BooleanField(default=False)

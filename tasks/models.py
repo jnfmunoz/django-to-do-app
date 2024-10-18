8d5e046bb007b34ac1d14b128a6eaf86d3e2f118
jnfmunoz <juanmunozgrau@gmail.com>
2024-10-18 00:44:48 -0300

    add-model

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,34 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 from django.urls import reverse
 
 # Create your models here.
-class Task(models.model):
-    titulo = models.CharField(min_length=100 ,max_length=200)
-    descripcion = models.TextField(min_length=100 ,max_length=200)
-    fecha_limite = models.DateField()
-    prioridad = models.CharField()
-    estado = models.BooleanField(default=False)
+class Task(models.Model):
+    PRIORITY = {
+        'Baja':'Baja',
+        'Media':'Media',
+        'Alta':'Alta',
+    }
+
+    STATUS = {
+         'Completada':'Completada',
+         'En Progreso':'En Progreso',
+    }
+    
+    title = models.CharField(validators=[MinLengthValidator(3)], max_length=200, verbose_name="Título")
+    description = models.TextField(max_length=200, verbose_name="Descripción")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha creación")
+    deadline = models.DateField(verbose_name="Fecha límite")
+    priority = models.CharField(max_length=12, choices=PRIORITY, blank=True, default='Baja', verbose_name="Prioridad")
+    status = models.CharField(max_length=12, choices=STATUS, blank=True, default='En Progreso', verbose_name="Estado")
+
+    class Meta:
+        verbose_name = "Tarea"
+        verbose_name_plural = "Tareas"
+        ordering = ['-created']
+
+    def get_absolute_url(self):
+        return reverse('task', args=[str(self.id)])
+
+    def __str__(self) -> str:
+        return self.title

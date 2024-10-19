000495a9028ab41328b1a4ecd8070f9f20ab47c8
jnfmunoz <juanmunozgrau@gmail.com>
2024-10-19 00:15:27 -0300

    crud-development

--- tasks/models.py
+++ tasks/models.py
@@ -5,22 +5,22 @@ from django.urls import reverse
 # Create your models here.
 class Task(models.Model):
     PRIORITY = {
-        'Baja':'Baja',
-        'Media':'Media',
-        'Alta':'Alta',
+        ('B','Baja'),
+        ('M','Media'),
+        ('A','Alta'),
     }
 
     STATUS = {
-         'Completada':'Completada',
-         'En Progreso':'En Progreso',
+         ('E','En Progreso'),
+         ('C','Completada'),
     }
     
     title = models.CharField(validators=[MinLengthValidator(3)], max_length=200, verbose_name="Título")
     description = models.TextField(max_length=200, verbose_name="Descripción")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha creación")
     deadline = models.DateField(verbose_name="Fecha límite")
-    priority = models.CharField(max_length=12, choices=PRIORITY, blank=True, default='Baja', verbose_name="Prioridad")
-    status = models.CharField(max_length=12, choices=STATUS, blank=True, default='En Progreso', verbose_name="Estado")
+    priority = models.CharField(max_length=1, choices=PRIORITY, blank=True, default='B', verbose_name="Prioridad")
+    status = models.CharField(max_length=12, choices=STATUS, blank=True, default='E', verbose_name="Estado")
 
     class Meta:
         verbose_name = "Tarea"
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    context = {'tasks': tasks, 'priorities': Task.PRIORITY ,'states': Task.STATUS}
+
+    return render(request, 'task_list.html', context)
+
+def task_detail(request, pk):
+    task = Task.objects.get(pk=pk)
+    context = {'task': task}
+    return render(request, 'task_detail.html', context)
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()            
+            return redirect('task-list')            
+    else:
+        form = TaskForm()
+    return render(request, 'task_create.html', {'form':form})
+
+def task_update(request, pk):
+    task = Task.objects.get(pk=pk)
+    form = TaskForm(request.POST, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('task-list')
+    else:
+        print(form.errors)
+    return render(request, 'task_update.html', {'task':task, 'priorities': Task.PRIORITY ,'states': Task.STATUS})

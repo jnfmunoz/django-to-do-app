b2722987d7e91fe6774d7ab3369ae397de0db8dc
jnfmunoz <juanmunozgrau@gmail.com>
2024-11-11 01:45:16 -0300

    confirm-delete-tasks-done

--- core/static/core/js/alerts.js
+++ core/static/core/js/alerts.js
@@ -71,5 +71,37 @@ $(document).ready(function(){
                 });
             });
         });
-    }
+    };
+
+    if(document.body.classList.contains('task-detail-page')) {
+        // Seleccionar todos los botones de eliminación
+        const deleteButtons = document.querySelectorAll('.deleteTaskBtn');
+
+        // Agregar evento de clic a cada botón de eliminación
+        deleteButtons.forEach(button => {
+            button.addEventListener('click', function(e) {
+                e.preventDefault(); // Evitar el comportamiento por defecto del botón
+                
+                // Obtener la URL del atributo data-url del botón clicado
+                const deleteUrl = this.getAttribute('data-url');
+                
+                // Mostrar la alerta de confirmación con SweetAlert
+                Swal.fire({
+                    title: "Eliminar Tarea",
+                    text: "¿Estás seguro de que deseas eliminar esta tarea?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#d33",
+                    cancelButtonColor: "#3085d6",
+                    confirmButtonText: "Sí, eliminar",
+                    cancelButtonText: "Cancelar"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Redirigir a la URL de eliminación si el usuario confirma
+                        window.location.href = deleteUrl;
+                    }
+                });
+            });
+        });
+    };
 });
